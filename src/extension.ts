6c159885bbe99643456de669b42ff58e840aefcb
Yigao Fan <yigao.fan@microfocus.com>
2021-11-17 15:12:29 +0800

    delete some code and fix semicolon

--- src/extension.ts
+++ src/extension.ts
@@ -1,8 +1,10 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import * as path from 'path'
-import * as fs from 'fs'
+import * as path from 'path';
+import * as fs from 'fs';
+
+const log = console.log.bind(console);
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -10,7 +12,7 @@ export function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "logview" is now active!');
+	log('Congratulations, your extension "logview" is now active!');
 
 	const cmd = 'logview.dogCoding.start';
 	let disp2 = vscode.commands.registerCommand(cmd, () => {
